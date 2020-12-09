e6c37a06e04eaa97af07ba844f456b8431ec866c
Vašek Henzl <vasek.henzl@gmail.com>
2020-12-09 20:05:02 +1300

    WIP: Allow specifying locale for `lexicographicSortSchema()`

--- src/utilities/lexicographicSortSchema.d.ts
+++ src/utilities/lexicographicSortSchema.d.ts
@@ -1,8 +1,16 @@
 import { GraphQLSchema } from '../type/schema';
 
+export interface CompareOptions {
+  locales?: string | Array<string>;
+  options?: Intl.CollatorOptions;
+}
+
 /**
  * Sort GraphQLSchema.
  *
  * This function returns a sorted copy of the given GraphQLSchema.
  */
-export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema;
+export function lexicographicSortSchema(
+  schema: GraphQLSchema,
+  compareOptions?: CompareOptions,
+): GraphQLSchema;
--- src/utilities/lexicographicSortSchema.js
+++ src/utilities/lexicographicSortSchema.js
@@ -33,15 +33,23 @@ import {
   isInputObjectType,
 } from '../type/definition';
 
+type CompareOptions = {|
+  locales?: string | Array<string>,
+  options?: Intl$CollatorOptions,
+|};
+
 /**
  * Sort GraphQLSchema.
  *
  * This function returns a sorted copy of the given GraphQLSchema.
  */
-export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema {
+export function lexicographicSortSchema(
+  schema: GraphQLSchema,
+  compareOptions?: CompareOptions,
+): GraphQLSchema {
   const schemaConfig = schema.toConfig();
   const typeMap = keyValMap(
-    sortByName(schemaConfig.types),
+    sortByName(schemaConfig.types, compareOptions),
     (type) => type.name,
     sortNamedType,
   );
@@ -49,7 +57,9 @@ export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema {
   return new GraphQLSchema({
     ...schemaConfig,
     types: objectValues(typeMap),
-    directives: sortByName(schemaConfig.directives).map(sortDirective),
+    directives: sortByName(schemaConfig.directives, compareOptions).map(
+      sortDirective,
+    ),
     query: replaceMaybeType(schemaConfig.query),
     mutation: replaceMaybeType(schemaConfig.mutation),
     subscription: replaceMaybeType(schemaConfig.subscription),
@@ -78,35 +88,47 @@ export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema {
     const config = directive.toConfig();
     return new GraphQLDirective({
       ...config,
-      locations: sortBy(config.locations, (x) => x),
+      locations: sortBy(config.locations, (x) => x, compareOptions),
       args: sortArgs(config.args),
     });
   }
 
   function sortArgs(args: GraphQLFieldConfigArgumentMap) {
-    return sortObjMap(args, (arg) => ({
-      ...arg,
-      type: replaceType(arg.type),
-    }));
+    return sortObjMap(
+      args,
+      (arg) => ({
+        ...arg,
+        type: replaceType(arg.type),
+      }),
+      compareOptions,
+    );
   }
 
   function sortFields(fieldsMap: GraphQLFieldConfigMap<mixed, mixed>) {
-    return sortObjMap(fieldsMap, (field) => ({
-      ...field,
-      type: replaceType(field.type),
-      args: sortArgs(field.args),
-    }));
+    return sortObjMap(
+      fieldsMap,
+      (field) => ({
+        ...field,
+        type: replaceType(field.type),
+        args: sortArgs(field.args),
+      }),
+      compareOptions,
+    );
   }
 
   function sortInputFields(fieldsMap: GraphQLInputFieldConfigMap) {
-    return sortObjMap(fieldsMap, (field) => ({
-      ...field,
-      type: replaceType(field.type),
-    }));
+    return sortObjMap(
+      fieldsMap,
+      (field) => ({
+        ...field,
+        type: replaceType(field.type),
+      }),
+      compareOptions,
+    );
   }
 
   function sortTypes<T: GraphQLNamedType>(arr: $ReadOnlyArray<T>): Array<T> {
-    return sortByName(arr).map(replaceNamedType);
+    return sortByName(arr, compareOptions).map(replaceNamedType);
   }
 
   function sortNamedType<T: GraphQLNamedType>(type: T) {
@@ -140,7 +162,7 @@ export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema {
       const config = type.toConfig();
       return new GraphQLEnumType({
         ...config,
-        values: sortObjMap(config.values),
+        values: sortObjMap(config.values, undefined, compareOptions),
       });
     }
     // istanbul ignore else (See: 'https://github.com/graphql/graphql-js/issues/2618')
@@ -157,9 +179,13 @@ export function lexicographicSortSchema(schema: GraphQLSchema): GraphQLSchema {
   }
 }
 
-function sortObjMap<T, R>(map: ObjMap<T>, sortValueFn?: (T) => R): ObjMap<R> {
+function sortObjMap<T, R>(
+  map: ObjMap<T>,
+  sortValueFn?: (T) => R,
+  compareOptions?: CompareOptions,
+): ObjMap<R> {
   const sortedMap = Object.create(null);
-  const sortedKeys = sortBy(Object.keys(map), (x) => x);
+  const sortedKeys = sortBy(Object.keys(map), (x) => x, compareOptions);
   for (const key of sortedKeys) {
     const value = map[key];
     sortedMap[key] = sortValueFn ? sortValueFn(value) : value;
@@ -169,17 +195,23 @@ function sortObjMap<T, R>(map: ObjMap<T>, sortValueFn?: (T) => R): ObjMap<R> {
 
 function sortByName<T: { +name: string, ... }>(
   array: $ReadOnlyArray<T>,
+  compareOptions?: CompareOptions,
 ): Array<T> {
-  return sortBy(array, (obj) => obj.name);
+  return sortBy(array, (obj) => obj.name, compareOptions);
 }
 
 function sortBy<T>(
   array: $ReadOnlyArray<T>,
   mapToKey: (T) => string,
+  compareOptions?: CompareOptions,
 ): Array<T> {
   return array.slice().sort((obj1, obj2) => {
     const key1 = mapToKey(obj1);
     const key2 = mapToKey(obj2);
-    return key1.localeCompare(key2);
+    return key1.localeCompare(
+      key2,
+      compareOptions?.locales,
+      compareOptions?.options,
+    );
   });
 }
